9f9487e3591bdfe56a7c9431aa642fe1a736eb9e
rettake <rettakecsgo@gmail.com>
2023-05-30 02:01:01 +0300

    -m (interface done)

--- script.js
+++ script.js
@@ -10,10 +10,36 @@ netmonetLinkElement = document.querySelector(".netmonet_link"); // Netmonet
 const calculateStyles = (val, index) => {
   console.log(val, index);
 
+  const getIconUrl = () => {
+    if (index == 0) {
+      return "icon_menu"
+    }
+
+    if (index == 1) {
+      return "inst_icon"
+    }
+
+    if (index == 2) {
+      return "rate_icon"
+    }
+
+    if (index == 3) {
+      return "tips_icon"
+    }
+  };
+
+  if (val < 50) {
+    sliderElements[index].style.setProperty("--opacityIcon", 1 - (val * 2) / 100);
+    sliderElements[index].style.setProperty("--linkIcon", `url(./images/${getIconUrl()}.svg)`);
+  } else if (val == 50) {
+    sliderElements[index].style.setProperty("--opacityIcon", 0);
+  } else {
+    sliderElements[index].style.setProperty("--opacityIcon", ((val % 50) * 2) / 100);
+    sliderElements[index].style.setProperty("--linkIcon", `url(./images/${getIconUrl()}2.svg)`);
+  }
+
   if (val > 10) {
-    buttonElements[
-      index
-    ].style.backgroundColor = `rgba(244, 244, 244, 0.${val})`;
+    buttonElements[index].style.backgroundColor = `rgba(244, 244, 244, 0.${val})`;
   } else {
     buttonElements[index].style.backgroundColor = `rgba(244, 244, 244, .1)`;
   }
