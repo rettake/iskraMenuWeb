6bf6f489f281cc46ab2416767405fbf0b0a5c06c
rettake <rettakecsgo@gmail.com>
2023-07-05 13:54:46 +0300

    -m anims

--- script.js
+++ script.js
@@ -32,23 +32,9 @@ buttonElements.forEach((e, index) => e.addEventListener('click', () => {
 buttonBackElement.addEventListener("click", () => {
   console.log("1");
 
-  // sliderElements[0].value = 0;
-  // sliderElements[1].value = 0;
-  // sliderElements[2].value = 0;
-  // sliderElements[3].value = 0;
-  // calculateStyles(sliderElements[0].value, 0);
-  // calculateStyles(sliderElements[1].value, 1);
-  // calculateStyles(sliderElements[2].value, 2);
-  // calculateStyles(sliderElements[3].value, 3);
-
   pdfElement.classList.remove("pages_show");
 
   buttonElements.forEach(e => e.classList.remove("middle_hide"))
-
-  // middleElements[0].classList.remove("middle_hide");
-  // middleElements[1].classList.remove("middle_hide");
-  // middleElements[2].classList.remove("middle_hide");
-  // middleElements[3].classList.remove("middle_hide");
 
   bodyElement.classList.add("overflow_hide");
 
